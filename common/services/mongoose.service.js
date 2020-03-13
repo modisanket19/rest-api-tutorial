e7850c83521f761fd159de57f8b29caca810fa40
Sanket Modi <modi_sanket@network.lilly.com>
2020-03-13 16:37:55 +0530

    Update mongoose.service.js

--- common/services/mongoose.service.js
+++ common/services/mongoose.service.js
@@ -15,7 +15,7 @@ const options = {
 };
 const connectWithRetry = () => {
     console.log('MongoDB connection with retry')
-    mongoose.connect("mongodb://mongo:27017/rest-tutorial", options).then(()=>{
+    mongoose.connect("mongodb+srv://mongo:27017@cluster0-7wcth.mongodb.net/test?retryWrites=true&w=majority", options).then(()=>{
         console.log('MongoDB is connected')
     }).catch(err=>{
         console.log('MongoDB connection unsuccessful, retry after 5 seconds. ', ++count);
